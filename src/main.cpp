0007fa8eb608d8bf4d3aa160c7ac2fde1c1cbe75
D-CarbajalS <a23310304@live.ceti.mx>
2024-11-26 00:18:53 +0000

    se formateo el codigo

--- src/main.cpp
+++ src/main.cpp
@@ -1,82 +1,85 @@
-#include<iostream>
-#include<deque>
+#include <iostream>
+#include <deque>
 
 using namespace std;
 
 class cPlayer
 {
-    public:
+public:
     int x, y;
-    cPlayer(int width) { x=width/2; y=0; }
+    cPlayer(int width)
+    {
+        x = width / 2;
+        y = 0;
+    }
 };
 class cLane
 {
-    private:
-    deque<bool>cars;
-    public:
+private:
+    deque<bool> cars;
+
+public:
     cLane(int width)
     {
-        for(int i=0; i<width; i++)
-        cars.push_front(false);
+        for (int i = 0; i < width; i++)
+            cars.push_front(false);
     }
     void Move()
     {
-        if(rand() %10==1)
-        cars.push_front(true);
+        if (rand() % 10 == 1)
+            cars.push_front(true);
         else
-        cars.push_front(false);
+            cars.push_front(false);
         cars.pop_back();
     }
-    bool CheckPos(int pos) {  return cars[pos]; } 
+    bool CheckPos(int pos) { return cars[pos]; }
 };
 class cGame
 {
-    private:
+private:
     bool quit;
     int numberOfLanes;
     int width;
     int score;
-    Cplayer*player;
-    vector<cLane*>map;
+    Cplayer *player;
+    vector<cLane *> map;
 
-    public:
-    cGame(int w=20, int h=10)
+public:
+    cGame(int w = 20, int h = 10)
     {
-        numberofLanes=h;
-        width= w;
-        quite=false;
-        for(int i=0; i<numberOfLanes; i++)
-        map.push_back(new cLane(width));
-        player=new cPLAYER(width);
+        numberofLanes = h;
+        width = w;
+        quite = false;
+        for (int i = 0; i < numberOfLanes; i++)
+            map.push_back(new cLane(width));
+        player = new cPLAYER(width);
     }
     void Draw()
     {
         system("cls");
-        for(int i=0; i<numberOfLanes; i++)
+        for (int i = 0; i < numberOfLanes; i++)
         {
-            for(int j=0; j<width; j++)
+            for (int j = 0; j < width; j++)
             {
-                if(map[i]->Checkpos(j))
-                cout<<"#";
-                else if(player -> x==j && player -> y ==i)
-                cout<<"V";
+                if (map[i]->Checkpos(j))
+                    cout << "#";
+                else if (player->x == j && player->y == i)
+                    cout << "V";
                 else
-                cout<< " ";
+                    cout << " ";
             }
-            cout<<endl;
+            cout << endl;
         }
     }
     void Input()
     {
-
     }
     void Logic()
     {
-
     }
     void Run()
     {
-        while(!quit)
+        while (!quit)
         {
             Input();
             Draw();
@@ -84,24 +87,24 @@ class cGame
         }
     }
 };
-int main ()
+int main()
 {
     cLane lane1(20);
-    for(int i=0; i<20; i++)
+    for (int i = 0; i < 20; i++)
     {
-        if(lane1.CheckPos(i))
-        cout<<"1";
+        if (lane1.CheckPos(i))
+            cout << "1";
         else
-        cout<<"0";
+            cout << "0";
     }
     lane1.Move();
-    cout<<endl;
-      for(int i=0; i<20; i++)
+    cout << endl;
+    for (int i = 0; i < 20; i++)
     {
-        if(lane1.CheckPos(i))
-        cout<<"1";
+        if (lane1.CheckPos(i))
+            cout << "1";
         else
-        cout<<"0";
+            cout << "0";
     }
     getchar();
     return 0;
